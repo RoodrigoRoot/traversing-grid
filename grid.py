5df798fee9bfce83f290a596775238f97326761e
roodrigo <roodrigoroot@gmail.com>
2020-12-14 13:33:08 -0600

    First commit

---
+++ grid.py
@@ -0,0 +1,10 @@
+
+test_cases = input("Please, input total test cases: ")
+
+
+for test in range(test_cases):
+    rows = input("Please enter the number of rows: ")
+    columns = input("Please enter the number of columns: ")
+    
+
+
