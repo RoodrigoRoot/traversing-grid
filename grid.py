9dd6b703556debfe745c959713b1272da867a3ce
roodrigo <roodrigoroot@gmail.com>
2020-12-14 16:07:28 -0600

    enter N and M

--- grid.py
+++ grid.py
@@ -1,10 +1,8 @@
 
 test_cases = input("Please, input total test cases: ")
-
+test_cases = int(test_cases)
 
 for test in range(test_cases):
-    rows = input("Please enter the number of rows: ")
-    columns = input("Please enter the number of columns: ")
-    
-
+    rows = input("Please enter the number of rows (N): ")
+    columns = input("Please enter the number of columns (M): ")
 
